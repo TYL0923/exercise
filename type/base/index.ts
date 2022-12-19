2306c0dc6abe91dfcf169f4cfd43fa470c801dad
tyl0923 <1833290014@qq.com>
2022-12-19 17:24:00 +0800

    refactor(type): all

--- type/base/index.ts
+++
@@ -1,8 +0,0 @@
-import type { IQuestionSet } from '../question-set'
-import type { IQuestion } from '../question'
-export type BaseReturnQuestionSet = Omit<
-  IQuestionSet,
-  'answerKeys'
-> & {
-  questions: IQuestion[]
-}
--- type/index.ts
+++ type/index.ts
@@ -1,5 +1,24 @@
-export * from './answer-key'
-export * from './question'
-export * from './question-set'
-export * from './user'
-export * from './base'
+export interface User {
+  account: string
+  name: string
+}
+export type QuestionType = 'select' | 'judge'
+export interface Question {
+  id: string
+  type: QuestionType
+  title: string
+  isDo: 0 | 1
+  isError: 0 | 1
+  testAnswer: string
+  exerciseAnswer: string
+  correctAnswer: string
+}
+export interface QuestionSet {
+  id: string
+  title: string
+  isActive: 0 | 1
+  num: number
+  createTime: string
+  author: User
+  questions: Question[]
+}
