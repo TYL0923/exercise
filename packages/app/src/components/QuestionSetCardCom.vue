8920e60add1af53c4270b13c6e89e4d3dba22d2d
tyl0923 <1833290014@qq.com>
2022-12-30 16:12:28 +0800

    chore(app): update QuestionSetCard component

--- packages/app/src/components/QuestionSetCardCom.vue
+++ packages/app/src/components/QuestionSetCardCom.vue
@@ -8,30 +8,57 @@ const props = defineProps<{
 const emits = defineEmits<{
   (e: 'click', questionSet: QuestionSet): void
 }>()
+const isDo = computed(() => {
+  return props.data.questions.reduce((pre, cur) => { return pre + (cur.isDo === 1 ? 1 : 0) }, 0)
+})
+const isError = computed(() => {
+  return props.data.questions.reduce((pre, cur) => { return pre + (cur.isError === 1 ? 1 : 0) }, 0)
+})
 const tagArr = computed(() => {
   return props.data.tags.length === 0 ? ['暂无标签'] : props.data.tags.split(',').slice(0, 3)
 })
 </script>
 
 <template>
-  <view p-4 bg-white flex flex-col gap-y-2 rounded-2 my-2 shadow @click="emits('click', data)">
-    <view flex items-center justify-between>
-      <view text-xl text-gray-900 font-medium>
-        {{ data.title }}
+  <view
+    p-4 bg-white my-2 rounded-2 shadow
+    flex flex-col
+    @click="emits('click', data)"
+  >
+    <view flex justify-between>
+      <view>
+        <view font-medium mb-2>
+          {{ data.title }}
+        </view>
+        <view>
+          <uni-tag
+            v-for="tag in tagArr" :key="tag"
+            mr-2 size="small" type="primary"
+            inverted :text="tag"
+          />
+        </view>
       </view>
-      <view text-sm text-gray-400>
-        {{ `${data.num} 题` }}
-      </view>
-    </view>
-    <view flex items-center gap-x-1>
-      <uni-tag v-for="tag in tagArr" :key="tag" size="small" type="primary" :inverted="true" :text="tag" />
-    </view>
-    <view flex items-center justify-between mt-2>
-      <view flex items-center>
-        <image w-60 h-60 rounded-full src="../static/logo.png" />
-        <view ml-2 text-sm text-gray-600>
+      <view flex flex-col items-center>
+        <image w-70 h-70 rounded-full src="../static/logo.png" />
+        <view text-xs text-gray-600 mt-2>
           {{ data.author.name }}
         </view>
+      </view>
+    </view>
+    <view mt-4>
+      <view flex items-center gap-x-3 text-xs text-gray-500>
+        <view>
+          {{ ` 共 ${data.num} 题 ` }}
+        </view>
+        <view>
+          {{ ` 已做 ${isDo} 题 ` }}
+        </view>
+        <view>
+          {{ ` 做错 ${isError} 题 ` }}
+        </view>
+      </view>
+      <view mt-1>
+        <progress :percent="100 * isDo / data.num" border-radius="4" activeColor="#007aff" />
       </view>
     </view>
   </view>
