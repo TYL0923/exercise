0dd5238a12954d7c94e66001b899306fa5317bda
tyl0923 <1833290014@qq.com>
2023-01-05 20:18:55 +0800

    feat(m): use hooks

---
+++ packages/mobile/src/composables/useJoinConfirm.ts
@@ -0,0 +1,90 @@
+import type { QuestionSet } from '@exercise/type'
+import { ActionSheet, Tag, Button as VantButton, showConfirmDialog, showNotify } from 'vant'
+import { QuestionSetCardCom } from '../components'
+import { joinQuestionSetById } from '../lib/api'
+import { useLoginState } from './useLogin'
+export function useJoinConfirm() {
+  const loginState = useLoginState()
+  const isShow = ref<boolean>(false)
+  const joinQuestionSet = ref<QuestionSet>()
+  function showJoin(questionSet: QuestionSet) {
+    joinQuestionSet.value = questionSet
+    isShow.value = true
+  }
+  async function handleJoinQuestionSet() {
+    if (!joinQuestionSet.value)
+      return
+    showConfirmDialog({
+      title: '确认',
+      message: '确认加入题库',
+    })
+      .then(async () => {
+        // on confirm
+        isShow.value = false
+        uni.showLoading({
+          title: '加入中',
+        })
+        const { err, data } = await joinQuestionSetById(joinQuestionSet.value!.id, loginState.account)
+        if (!err && data) {
+          showNotify({
+            type: 'success',
+            message: '加入成功',
+            duration: 350,
+          })
+        }
+        else {
+          showNotify({
+            type: 'danger',
+            message: '加入失败',
+            duration: 350,
+          })
+        }
+        // initjoinableQuestionSetList('')
+        uni.hideLoading()
+      })
+      .catch(() => {
+        // on cancel
+      })
+  }
+  const joinConfirmCom = defineComponent({
+    name: 'JoinConfirmCom',
+    components: { ActionSheet, VantButton, QuestionSetCardCom, Tag },
+    setup() {
+      return () => h(
+        ActionSheet,
+        {
+          'show': isShow.value,
+          'onUpdate:show': (value) => { isShow.value = value },
+        },
+        () => h('div', { style: { padding: '16px' } },
+          [
+            h('div', {
+              style: {
+                padding: '0 16px 0 16px',
+                margin: '16px 0 16px 0',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              },
+            }, [
+              h(VantButton, { onClick: () => isShow.value = false }, () => '取消'),
+              h(VantButton, {
+                type: 'primary',
+                onClick: handleJoinQuestionSet,
+              }, () => '加入'),
+            ]),
+            joinQuestionSet.value
+              ? h(QuestionSetCardCom, {
+                data: joinQuestionSet.value,
+                isShadow: false,
+              })
+              : '',
+          ]),
+      )
+    },
+  })
+  return {
+    showJoin,
+    joinConfirmCom,
+  }
+}
---
+++ packages/mobile/src/composables/useStart.ts
@@ -0,0 +1,124 @@
+import type { QuestionSet } from '@exercise/type'
+import { ActionSheet, Button as VantButton } from 'vant'
+import { FilterCom, QuestionSetCardCom } from '../components'
+import { resetQuestion } from '../lib/api'
+import { useLoginState } from './useLogin'
+export function useStart() {
+  const loginState = useLoginState()
+  const isShow = ref<boolean>(false)
+  const filter = reactive<{
+    mode: 'exercise' | 'test'
+    start: 'continue' | 'restart'
+    part: 'all' | 'error' | 'not'
+  }>({
+    mode: 'test',
+    start: 'continue',
+    part: 'all',
+  })
+  const startQuestionSet = ref<QuestionSet>()
+  const filterOptions = reactive({
+    mode: [
+      {
+        label: '刷题模式',
+        key: 'exercise',
+      },
+      {
+        label: '考试模式',
+        key: 'test',
+      },
+    ],
+    start: [
+      {
+        label: '继续刷题',
+        key: 'continue',
+      },
+      {
+        label: '重新开始',
+        key: 'restart',
+      },
+    ],
+    part: [
+      {
+        label: '全部',
+        key: 'all',
+      },
+      {
+        label: '错题',
+        key: 'error',
+      },
+      {
+        label: '未做',
+        key: 'not',
+      },
+    ],
+  })
+  function showStart(questionSet: QuestionSet) {
+    startQuestionSet.value = questionSet
+    isShow.value = true
+  }
+  async function start() {
+    if (!startQuestionSet.value?.id)
+      return
+    if (filter.mode === 'test') {
+      uni.showLoading({
+        title: '生成试卷中',
+      })
+      await resetQuestion(startQuestionSet.value.id, loginState.account, 'test')
+      uni.hideLoading()
+      uni.navigateTo({
+        url: `/pages/test?id=${startQuestionSet.value.id}`,
+      })
+    }
+    else {
+      if (filter.start === 'restart') {
+        uni.showLoading({
+          title: '加载中',
+        })
+        await resetQuestion(startQuestionSet.value.id, loginState.account, 'exercise')
+        uni.hideLoading()
+      }
+      uni.navigateTo({
+        url: `/pages/exercise?id=${startQuestionSet.value.id}&part=${filter.part}`,
+      })
+    }
+  }
+  const StartCom = defineComponent({
+    name: 'StartCom',
+    components: {
+      ActionSheet,
+      VantButton,
+      FilterCom,
+      QuestionSetCardCom,
+    },
+    setup() {
+      return () => h(
+        ActionSheet,
+        {
+          'show': isShow.value,
+          'onUpdate:show': (value) => { isShow.value = value },
+        },
+        () => h('div', { style: { padding: '16px' } }, [
+          h('div', {
+            style: {
+              borderBottom: '1px solid #e5e7eb',
+              padding: '0 16px 16px 16px',
+              margin: '16px 0 0 0',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            },
+          }, [
+            h(VantButton, { onClick: () => isShow.value = false }, () => '取消'),
+            h(VantButton, { type: 'primary', onClick: start }, () => '开始'),
+          ]),
+          h('div', { style: { height: '100px' } }, h(FilterCom, { filter, options: filterOptions })),
+          startQuestionSet.value ? h(QuestionSetCardCom, { data: startQuestionSet.value, isShadow: false }) : '',
+        ]),
+      )
+    },
+  })
+  return {
+    showStart,
+    StartCom,
+  }
+}
